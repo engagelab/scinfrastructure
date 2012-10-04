19f3d22cb65aaef83c8a33b7b781f038d7644c56
Fahied <fahied@gmail.com>
2012-10-04 09:20:33 +0200

    added Icons for the Toolbars added new controller for Portfolio

--- app/controllers/SProjects.java
+++ app/controllers/SProjects.java
@@ -4,7 +4,6 @@ import static play.libs.Json.toJson;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
