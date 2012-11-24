f31c36451e5c7c397f450f6cc16c254a3f09c528
Fahied <fahied@gmail.com>
2012-11-24 15:08:49 +0100

    iphone Menu Services added

--- app/controllers/SProjects.java
+++ app/controllers/SProjects.java
@@ -18,33 +18,78 @@ import play.mvc.*;
  */
 
 public class SProjects extends Controller {
-	
+
 	/*
 	 * 
 	 * Project Services
-	 * 
-	 * */
-	
+	 */
+
 	public static Result fetchAllProjects() {
 
 		List<SProject> groups = SProject.find.asList();
-	    response().setContentType("application/json");
-	     
+		response().setContentType("application/json");
+
 		return ok(toJson(groups));
 	}
-	
+
 	/*
-	 * Map group = new HashMap();
-				group.put("name", "NewYork");
-				group.put("runId", 2);
-				
-				JsonNode node = Json.toJson(group);
-	 * */
-	
-	
-	
-	
+	 * Map group = new HashMap(); group.put("name", "NewYork");
+	 * group.put("runId", 2);
+	 * 
+	 * JsonNode node = Json.toJson(group);
+	 */
+
 	public static Result fetchMenuItems() {
+
+		SProject menu = SProject.find.get();
+
+		if (menu == null) {
+			int empty[] = null;
+			return ok(toJson(empty));
+		}
+		List<SScene> scenes = menu.sscenes;
+		List<STask> tasks = menu.stasks;
+
+		List<Map<String, Object>> menuMap = new LinkedList<Map<String, Object>>();
+
+		for (SScene scene : scenes) {
+			Map<String, Object> sceneItem = new HashMap<String, Object>();
+			sceneItem.put("id", scene.id);
+			sceneItem.put("title", scene.title);
+			sceneItem.put("icon", scene.icon);
+
+			List<Map<String, String>> taskMap = new LinkedList<Map<String, String>>();
+			if (tasks != null) {
+
+				for (STask task : tasks) {
+					Map<String, String> taskItem = new HashMap<String, String>();
+
+					if (task.sceneId.equals(sceneItem.get("id"))) {
+						// System.out.println("task found");
+						// if (taskItem.size() >= 1)
+						{
+							taskItem.put("id", task.id);
+							taskItem.put("title", task.title);
+							taskItem.put("description", task.description);
+							taskItem.put("taskType", task.taskType);
+							taskItem.put("icon", task.icon);
+							taskMap.add(taskItem);
+						}
+					}
+					sceneItem.put("stasks", taskMap);
+				}
+
+			}
+
+			menuMap.add(sceneItem);
+		}
+
+		response().setContentType("application/json");
+
+		return ok(toJson(menuMap));
+	}
+
+	public static Result menuForIphone() {
 
 		SProject menu = SProject.find.get();
 		
@@ -61,32 +106,42 @@ public class SProjects extends Controller {
 		for(SScene scene : scenes)
 		{
 			Map<String, Object> sceneItem = new HashMap<String, Object>();
-			sceneItem.put("id", scene.id);
-			sceneItem.put("title", scene.title);
-			sceneItem.put("icon", scene.icon);
-			
-			List<Map<String, String>> taskMap = new LinkedList<Map<String, String>>();
-			if (tasks != null) {
+			//FIXME  Bad programming pattern to put logic against a variable
+			if(scene.title.equals("Avisartikkel") || scene.title.equals("Mappe"))
+			{
+				continue;
+			}
+			else 
+			{
+				sceneItem.put("id", scene.id);
+				sceneItem.put("title", scene.title);
+				sceneItem.put("icon", scene.icon);
+				
+				List<Map<String, String>> taskMap = new LinkedList<Map<String, String>>();
+				if (tasks != null) {
 
-				for(STask task : tasks)
-				{
-					Map<String, String> taskItem = new HashMap<String, String>();
-					
-					if ( task.sceneId.equals(sceneItem.get("id"))) 
+					for(STask task : tasks)
 					{
-						//System.out.println("task found");
-						//if (taskItem.size() >= 1) 
+						Map<String, String> taskItem = new HashMap<String, String>();
+						
+						if ( task.sceneId.equals(sceneItem.get("id"))) 
 						{
-							taskItem.put("id", task.id);
-							taskItem.put("title", task.title);
-							taskItem.put("description", task.description);
-							taskItem.put("taskType", task.taskType);
-							taskItem.put("icon", task.icon);
-							taskMap.add(taskItem);
+							//System.out.println("task found");
+							//if (taskItem.size() >= 1) 
+							{
+								taskItem.put("id", task.id);
+								taskItem.put("title", task.title);
+								taskItem.put("description", task.description);
+								taskItem.put("taskType", task.taskType);
+								taskItem.put("icon", task.icon);
+								taskMap.add(taskItem);
+							}
 						}
+						sceneItem.put("stasks", taskMap);
 					}
-					sceneItem.put("stasks", taskMap);
-				}
+				
+			}
+			
 				
 			}
 
@@ -98,89 +153,58 @@ public class SProjects extends Controller {
 		return ok(toJson(menuMap));
 	}
 
-	
-	
-	
-	
-	
-	
-	
 	public static Result addProject() {
-		
+
 		// parse JSON from request body
 		JsonNode node = ctx().request().body().asJson();
 		String title = node.get("title").asText();
-				
+
 		SProject project = new SProject(title);
 		project.save();
-		
+
 		return ok(toJson(project));
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	public static Result fetchProjectById( String projectId) {
-		
+
+	public static Result fetchProjectById(String projectId) {
+
 		SProject project = SProject.find.byId(projectId);
 		return ok(toJson(project));
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	public static Result fetchProjectByTitle( String title) {
-		
+
+	public static Result fetchProjectByTitle(String title) {
+
 		SProject project = SProject.find.filter("title", title).get();
 		return ok(toJson(project));
-		
+
 	}
-	
-	
-	
-	
+
 	/*
 	 * 
 	 * Act Service
-	 * 
-	 * */
+	 */
 	public static Result fetchActs(String projectId) {
-		
+
 		SProject project = SProject.find.byId(projectId);
 		List<SAct> acts = project.sacts;
 		return ok(toJson(acts));
-		
+
 	}
-	
-	
-	
-	/* POST - JSON request
-	
-		
-	{
-		"title"  : "Miracle"
-	}
-	
-	
-	*/
-	
+
+	/*
+	 * POST - JSON request
+	 * 
+	 * 
+	 * { "title" : "Miracle" }
+	 */
+
 	public static Result addAct(String projectId) {
-		
+
 		// parse JSON from request body
 		JsonNode node = ctx().request().body().asJson();
 		String title = node.get("title").asText();
-				
+
 		SProject project = SProject.find.byId(projectId);
 		SAct act = new SAct(title);
 		if (project.sacts == null) {
@@ -188,89 +212,73 @@ public class SProjects extends Controller {
 		}
 		project.addAct(act);
 		project.save();
-		
+
 		return ok(toJson(act));
-		
+
 	}
-	
-	
-	
-	
+
 	/*
 	 * 
 	 * Scene Service
-	 * 
-	 * */
+	 */
 	public static Result fetchScenes(String projectId) {
-		
+
 		SProject project = SProject.find.byId(projectId);
 		List<SScene> scenes = project.sscenes;
 		return ok(toJson(scenes));
-		
+
 	}
-	
-	
-	
-	/* POST - JSON request
-	
-	
-	{
-		"title"  : "Miracle"
-	}
-	
-	
-	*/
-	
+
+	/*
+	 * POST - JSON request
+	 * 
+	 * 
+	 * { "title" : "Miracle" }
+	 */
+
 	public static Result addScene(String projectId) {
-		
+
 		// parse JSON from request body
 		JsonNode node = ctx().request().body().asJson();
-		
+
 		String title = node.get("title").asText();
 		String icon = node.get("icon").asText();
 		String actId = node.get("actId").asText();
-				
+
 		SProject project = SProject.find.byId(projectId);
-		SScene scene = new SScene(title,icon, actId);
+		SScene scene = new SScene(title, icon, actId);
 		if (project.sscenes == null) {
 			project.sscenes = new ArrayList<SScene>();
 		}
 		project.addScene(scene);
 		project.save();
-		
+
 		return ok(toJson(scene));
-		
+
 	}
-	
-	
-	
+
 	/*
 	 * 
 	 * Task Service
-	 * 
-	 * */
+	 */
 	public static Result fetchTasksByProject(String projectId) {
-		
+
 		SProject project = SProject.find.byId(projectId);
 		if (project.stasks == null) {
-			project.stasks = new ArrayList<STask>();;
+			project.stasks = new ArrayList<STask>();
+			;
 		}
 		List<STask> tasks = project.stasks;
 		return ok(toJson(tasks));
 	}
-	
-	
-	
-	/* POST - JSON request
-	
-	
-	{
-		"title"  : "Miracle"
-	}
-	
-	
-	*/
-	
+
+	/*
+	 * POST - JSON request
+	 * 
+	 * 
+	 * { "title" : "Miracle" }
+	 */
+
 	public static Result addTask(String projectId) {
 
 		// parse JSON from request body
@@ -282,8 +290,8 @@ public class SProjects extends Controller {
 		String icon = node.get("icon").asText();
 
 		SProject project = SProject.find.byId(projectId);
-		
-		STask task = new STask(title, description, taskType, icon,sceneId);
+
+		STask task = new STask(title, description, taskType, icon, sceneId);
 		if (project.stasks == null) {
 			project.stasks = new ArrayList<STask>();
 		}
@@ -293,21 +301,18 @@ public class SProjects extends Controller {
 		return ok(toJson(task));
 
 	}
-	
-	
-	
-	
-	
+
 	public static Result fetchTaskById(String taskId) {
-		
-		//SProject project =  SProject.find.disableValidation().field("stasks.id").equal(taskId).get();
-		//There is only one project in our experiment
-		SProject project =  SProject.find.get();
+
+		// SProject project =
+		// SProject.find.disableValidation().field("stasks.id").equal(taskId).get();
+		// There is only one project in our experiment
+		SProject project = SProject.find.get();
 
 		if (project.stasks == null) {
 			project.stasks = new ArrayList<STask>();
 		}
-		
+
 		STask task = null;
 		for (STask p : project.stasks) {
 			if (p.id.equals(taskId)) {
@@ -319,14 +324,14 @@ public class SProjects extends Controller {
 			return ok();
 		}
 		return ok(toJson(task));
-		
+
 	}
-	
-	
+
 	public static Result getNameOfTask(String taskId) {
-		
-		SProject project =  SProject.find.disableValidation().field("stasks.id").equal(taskId).get();
-		
+
+		SProject project = SProject.find.disableValidation().field("stasks.id")
+				.equal(taskId).get();
+
 		STask task = null;
 		for (STask p : project.stasks) {
 			if (p.id.equals(taskId)) {
@@ -336,11 +341,12 @@ public class SProjects extends Controller {
 		}
 		return ok(task.title);
 	}
-	
+
 	public static String getNameOfTaskString(String taskId) {
-		
-		SProject project =  SProject.find.disableValidation().field("stasks.id").equal(taskId).get();
-		
+
+		SProject project = SProject.find.disableValidation().field("stasks.id")
+				.equal(taskId).get();
+
 		STask task = null;
 		for (STask p : project.stasks) {
 			if (p.id.equals(taskId)) {
@@ -350,7 +356,5 @@ public class SProjects extends Controller {
 		}
 		return task.title;
 	}
-	
-
 
 }
