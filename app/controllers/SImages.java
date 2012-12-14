4b302702b551b11914b7bf186eb637a69f313aea
Jeremy Toussaint <jt151077@gmail.com>
2012-12-14 09:53:40 +0100

    teacher image post service

--- app/controllers/SImages.java
+++ app/controllers/SImages.java
@@ -1,11 +1,12 @@
 package controllers;
 
-
 import static play.libs.Json.toJson;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 import org.apache.commons.io.IOUtils;
@@ -18,22 +19,17 @@ import com.mongodb.gridfs.GridFSDBFile;
 
 import models.*;
 
-
 import play.mvc.Controller;
 import play.mvc.Result;
 import play.mvc.Results;
 import play.mvc.Http.MultipartFormData.FilePart;
 import utils.GridFsHelper;
 
-
-
 /**
  * @author Muhammad Fahied
  */
 
 public class SImages extends Controller {
-
-	
 
 	public static Result fetchImagesById(String imageId) {
 
@@ -53,12 +49,6 @@ public class SImages extends Controller {
 
 	}
 
-	
-	
-	
-	
-	
-	
 	public static Result fetchImagesByGroupId(String groupId) {
 		SGroup group = SGroup.find.byId(groupId);
 		List<SImage> images = group.simages;
@@ -68,162 +58,161 @@ public class SImages extends Controller {
 			return ok(toJson(images));
 	}
 
+	//
+	// private static final Form<SImage> uploadForm = form(SImage.class);
+	//
+	// public static Result showBlank(){
+	// return ok(upload.render(uploadForm));
+	// }
 
-	
-	
-	
-//	
-//	private static final Form<SImage> uploadForm = form(SImage.class);
-//
-//	public static Result showBlank(){
-//		return ok(upload.render(uploadForm));
-//		}
-	
-	
 	public static Result addImage(String groupId, String taskId, String runId) {
 
-		
-//
-//	    if (filledForm.hasErrors()) {
-//	        return badRequest(views.html.upload.render(filledForm));
-//	    } else {
-//	        UploadResource resource = filledForm.get();
-//	        MultipartFormData body = request().body().asMultipartFormData();
-//	        FilePart resourceFile = body.getFile("resourceFile");
-//
-//	        /* Check resourceFile for null, then extract the File object and process it */
-//	     }
-		//String parm = ctx().request().body().asMultipartFormData().asFormUrlEncoded().toString();
-	   // int runIdINT = Integer.getInteger(runId);
-		FilePart filePart = ctx().request().body().asMultipartFormData().getFile("picture");
+		//
+		// if (filledForm.hasErrors()) {
+		// return badRequest(views.html.upload.render(filledForm));
+		// } else {
+		// UploadResource resource = filledForm.get();
+		// MultipartFormData body = request().body().asMultipartFormData();
+		// FilePart resourceFile = body.getFile("resourceFile");
+		//
+		// /* Check resourceFile for null, then extract the File object and
+		// process it */
+		// }
+		// String parm =
+		// ctx().request().body().asMultipartFormData().asFormUrlEncoded().toString();
+		// int runIdINT = Integer.getInteger(runId);
+		FilePart filePart = ctx().request().body().asMultipartFormData()
+				.getFile("picture");
 		SImage image = null;
 
 		if (filePart.getFile() == null)
 			return ok(toJson("{status: No Image found}"));
 		try {
-				image = new SImage(filePart.getFile(),filePart.getFilename(),filePart.getContentType(), taskId);
-				SGroup group = SGroup.find.byId(groupId);
-	
+			image = new SImage(filePart.getFile(), filePart.getFilename(),
+					filePart.getContentType(), taskId);
+			SGroup group = SGroup.find.byId(groupId);
+
+			if (group.simages == null) {
+				group.simages = new ArrayList<SImage>();
+			}
+
+			group.addImage(image);
+			group.save();
+		}
+
+		catch (IOException e) {
+			flash("uploadError", e.getMessage());
+		}
+
+		return ok(toJson(image));
+	}
+
+	public static Result addTeacherImageByTaskId(String taskId) {
+
+		FilePart filePart = ctx().request().body().asMultipartFormData()
+				.getFile("picture");
+		SImage image = null;
+
+		if (filePart.getFile() == null)
+			return ok(toJson("{status: No Image found}"));
+		try {
+			image = new SImage(filePart.getFile(), filePart.getFilename(),
+					filePart.getContentType(), taskId);
+
+			final int runId = 3;
+			List<SGroup> groups = SGroup.find.filter("runId", runId).asList();
+
+			for (SGroup group : groups) {
 				if (group.simages == null) {
 					group.simages = new ArrayList<SImage>();
 				}
-	
+
 				group.addImage(image);
 				group.save();
-			} 
-		
+			}
+		}
 		catch (IOException e) {
-				flash("uploadError", e.getMessage());
-				}
-		
+			flash("uploadError", e.getMessage());
+		}
+
 		return ok(toJson(image));
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 	public static Result showImage(String imageId) throws IOException {
-		
+
 		GridFSDBFile file = GridFsHelper.getFile(imageId);
-		
+
 		byte[] bytes = IOUtils.toByteArray(file.getInputStream());
-		
+
 		return Results.ok(bytes).as(file.getContentType());
-		
+
 	}
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	// {"imageId":"3423j342kjl23h1", "wxpos":120, "wypos":32}
 	public static Result updateImage() {
-		
+
 		JsonNode node = ctx().request().body().asJson();
 		String imageId = node.get("id").asText();
-		
+
 		if (SGroup.find.field("simages.id").equal(imageId).get() == null) {
 			return status(401, "Not Authorized");
-		} 
-		
+		}
+
 		int xpos = node.get("xpos").asInt();
 		int ypos = node.get("ypos").asInt();
-		Boolean isPortfolio =node.get("isPortfolio").asBoolean();
-		Boolean isFinalPortfolio =node.get("isFinalPortfolio").asBoolean();
-		
+		Boolean isPortfolio = node.get("isPortfolio").asBoolean();
+		Boolean isFinalPortfolio = node.get("isFinalPortfolio").asBoolean();
+
 		Query<SGroup> query = SGroup.datastore.createQuery(SGroup.class)
 				.field("simages.id").equal(imageId);
-		UpdateOperations<SGroup> ops = SGroup.datastore.createUpdateOperations(SGroup.class).disableValidation()
-				.set("simages.$.xpos", xpos)
-				.set("simages.$.ypos", ypos)
+		UpdateOperations<SGroup> ops = SGroup.datastore
+				.createUpdateOperations(SGroup.class).disableValidation()
+				.set("simages.$.xpos", xpos).set("simages.$.ypos", ypos)
 				.set("simages.$.isPortfolio", isPortfolio)
 				.set("simages.$.isFinalPortfolio", isFinalPortfolio);
-		
+
 		SGroup.datastore.findAndModify(query, ops);
 
 		return status(200, "OK");
-		
+
 	}
 
-	
-	
-	
-	public static Result deleteImageById(String imageId) throws MongoException, IOException {
+	public static Result deleteImageById(String imageId) throws MongoException,
+			IOException {
 
 		SGroup group = SGroup.find.filter("simages.id", imageId).get();
 		// Second locate the fruit and remove it:
-		for (SImage p : group.simages) 
-		{
-			if (p.id.equals(imageId)) 
-			{
-				//delete file from gridFS
+		for (SImage p : group.simages) {
+			if (p.id.equals(imageId)) {
+				// delete file from gridFS
 				p.deleteImage(p.fileId);
-				//Remove meta info and Group Document
+				// Remove meta info and Group Document
 				group.simages.remove(p);
 				group.save();
 				break;
 			}
 		}
-		
+
 		return ok("deleted successfully");
 	}
-		
-	
-	
-	
-	
-	
+
 	public static Result postCommentOnImage() {
 
 		JsonNode node = ctx().request().body().asJson();
 		String imageId = node.get("imageId").asText();
 		String content = node.get("content").asText();
 
-	
 		// Second locate the fruit and remove it:
 		SComment comment = new SComment(content);
 		// update member of embedded object list
 		Query<SGroup> query = SGroup.datastore.createQuery(SGroup.class)
 				.field("simages.id").equal(imageId);
-		UpdateOperations<SGroup> ops = SGroup.datastore.createUpdateOperations(SGroup.class).disableValidation()
+		UpdateOperations<SGroup> ops = SGroup.datastore
+				.createUpdateOperations(SGroup.class).disableValidation()
 				.add("simages.$.scomments", comment);
-		
+
 		SGroup group = SGroup.datastore.findAndModify(query, ops);
 
-		
 		SImage image = null;
 		for (SImage p : group.simages) {
 			if (p.id.equals(imageId)) {
@@ -232,15 +221,9 @@ public class SImages extends Controller {
 			}
 		}
 		return ok(toJson(image));
-		
+
 	}
-	
-	
-	
-	
-	
-	
-	
+
 	public static Result fetchCommentsByImage(String imageId) {
 
 		SGroup group = SGroup.find.filter("simages.id", imageId).get();
@@ -259,22 +242,14 @@ public class SImages extends Controller {
 		}
 		return ok(toJson(comments));
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	public static String getFileExtension(String filePath){  
-		  StringTokenizer stk=new StringTokenizer(filePath,".");  
-		  String FileExt="";  
-		  while(stk.hasMoreTokens()){  
-		   FileExt=stk.nextToken();  
-		  }  
-		  return FileExt;  
-		 } 
-	
+
+	public static String getFileExtension(String filePath) {
+		StringTokenizer stk = new StringTokenizer(filePath, ".");
+		String FileExt = "";
+		while (stk.hasMoreTokens()) {
+			FileExt = stk.nextToken();
+		}
+		return FileExt;
+	}
 
 }
